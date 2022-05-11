ff012ee325a1a159408710fb19281f64ccd97034
Wellington Garcia <garciawell@gmail.com>
2022-05-10 23:12:41 -0300

    passing data template

--- template/main.go
+++ template/main.go
@@ -6,24 +6,16 @@ import (
 	"os"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html"))
+}
+
 func main() {
-	tpl, err := template.ParseFiles("index.gohtml")
+	err := tpl.ExecuteTemplate(os.Stdout, "index.html", 452)
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
-
-	nf, err := os.Create("index.html")
-
-	if err != nil {
-		log.Println("error creating file", err)
-	}
-
-	defer nf.Close()
-
-	err = tpl.Execute(nf, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 }
